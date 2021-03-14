b0192bd05096103ea87adf686485259d1ee27f2a
AyersZ70879 <68571244+AyersZ70879@users.noreply.github.com>
2021-03-15 09:35:52 +1300

    Boundary testing errors fixed but buttons have no affect

--- 01_start_GUI.py
+++ 01_start_GUI.py
@@ -15,7 +15,7 @@ class Start:
         self.starting_rounds.set(0)
 
         # Quiz Heading (row 0)
-        self.quiz_box_label = Label(self.start_frame, text="Country - Capital Game", font="Arial 19 bold", bg="#DDF0FF")
+        self.quiz_box_label = Label(self.start_frame, text="Country - Capital Game", font="Arial 19 bold")
         self.quiz_box_label.grid(row=0)
 
         # Quiz Box Description (row 1)
@@ -37,7 +37,7 @@ class Start:
         self.add_rounds_button.grid(row=0, column=1)
 
         self.amount_error_label = Label(self.entry_error_frame, fg="maroon", text="", font="Arial 10 bold",
-                                        wrap=275, justify=LEFT, bg="#DDF0FF")
+                                        wrap=275, justify=LEFT)
         self.amount_error_label.grid(row=1, columnspan=2, pady=5)
 
         # Play
@@ -70,31 +70,31 @@ class Start:
             if starting_rounds < 1:
                 has_errors = "yes"
                 error_feedback = "Sorry, the least amount of rounds you can play is 1"
-            elif starting_balance > 10:
+            elif starting_rounds > 10:
                 has_errors = "yes"
                 error_feedback = "The most amount of rounds you can play is 10"
+                
+            else:
+                self.play_button.config(state=NORMAL)
 
         except ValueError:
             has_errors = "yes"
             error_feedback = "Please enter an integer (not text or decimals)"
 
         if has_errors == "yes":
-            self.start_amounts_entry.config(bg=error_back)
+            self.start_amount_entry.config(bg=error_back)
             self.amount_error_label.config(text=error_feedback)
 
         else:
             # set starting balance to amount entered by user
-            self.starting_funds.set(starting_rounds)
+            self.starting_rounds.set(starting_rounds)
             self.quiz_rounds_accept = Label(self.entry_error_frame, text=
             "You will be played {} rounds.".format(starting_rounds),
-                                           font="Arial 10 italic", wrap=275,
-                                           justify=LEFT, padx=10, pady=10)
-            self.quiz_rounds_accept.grid(row=2)
+                                           font="Arial 10 italic")
+
 
     # to_game function
-    def to_game(self, stakes):
-
-
+    def to_game(self, starting_rounds):
         starting_rounds = self.starting_rounds.get()
 
         Game(self, starting_rounds)
