d9a602069d0841cdd9b5727be67cd13034c9453c
AyersZ70879 <68571244+AyersZ70879@users.noreply.github.com>
2021-03-25 11:59:49 +1300

    Created compiled game to try and integrate rounds

--- 04_complied_assessment_game_v1.py
+++ 04_complied_assessment_game_v1.py
@@ -3,39 +3,115 @@ from functools import partial   # To prevent unwanted windows
 import random
 import csv
 
-rounds = 0
-
-
 class Start:
     def __init__(self, parent):
 
-        # GUI to get starting balances and stakes
+        # GUI to get number of rounds
         self.start_frame = Frame(padx=10, pady=10)
         self.start_frame.grid()
 
-        # Play Button
-        self.push_me_button = Button(self.start_frame, text="Push Me", command=self.to_game)
-        self.push_me_button.grid(row=0, pady=10)
+        # Set initial balance to zero
+        self.starting_rounds = IntVar()
+        self.starting_rounds.set(0)
 
+        # Quiz Heading (row 0)
+        self.quiz_box_label = Label(self.start_frame, text="Country - Capital Game", font="Arial 19 bold")
+        self.quiz_box_label.grid(row=0)
+
+        # Quiz Box Description (row 1)
+        self.quiz_instructions = Label(self.start_frame, text="Please enter the number of rounds "
+                                                              "you would like to play (between 1-10) and "
+                                                              "press the 'Start' button to play.", font="Arial 10 italic", wrap=275,
+                                          justify=LEFT, padx=10, pady=10)
+        self.quiz_instructions.grid(row=1)
+
+        # Entry box and error frame (row 2)
+        self.entry_error_frame = Frame(self.start_frame, width=200)
+        self.entry_error_frame.grid(row=2)
+
+        self.start_amount_entry = Entry(self.entry_error_frame, font="Arial 16 bold")
+        self.start_amount_entry.grid(row=0, column=0)
+
+        self.add_rounds_button = Button(self.entry_error_frame, font="Arial 14 bold", text="Check",
+                                       command=self.check_rounds)
+        self.add_rounds_button.grid(row=0, column=1)
+
+        self.amount_error_label = Label(self.entry_error_frame, fg="maroon", text="", font="Arial 10 bold",
+                                        wrap=275, justify=LEFT)
+        self.amount_error_label.grid(row=1, columnspan=2, pady=5)
+
+        # Play
+        self.play_frame = Frame(padx=5, pady=5, width=200)
+        self.play_frame.grid(row=3)
+
+        self.play_button = Button(self.play_frame, text="Play", command=self.to_game,
+                                        font="Arial 12 bold", bg="#87986a")
+        self.play_button.grid(row=0, column=0, pady=10)
+
+        # Disable play button at start
+        self.play_button.config(state=DISABLED)
+
+    # check rounds function
+    def check_rounds(self):
+        starting_rounds_set = self.start_amount_entry.get()
+
+
+        # Set error background and colours (and assume no errors at the start
+        error_back = "#ffafaf"
+        has_errors = "no"
+
+        # Change background to white (fr testing purposes)
+        self.start_amount_entry.config(bg="white")
+        self.amount_error_label.config(text="")
+
+        # If error disable button
+        self.play_button.config(state=DISABLED)
+
+        try:
+            starting_rounds_set = int(starting_rounds_set)
+
+            if starting_rounds_set < 1:
+                has_errors = "yes"
+                error_feedback = "Sorry, the least amount of rounds you can play is 1"
+            elif starting_rounds_set > 10:
+                has_errors = "yes"
+                error_feedback = "The most amount of rounds you can play is 10"
+
+            else:
+                self.play_button.config(state=NORMAL)
+
+        except ValueError:
+            has_errors = "yes"
+            error_feedback = "Please enter an integer (not text or decimals)"
+
+        if has_errors == "yes":
+            self.start_amount_entry.config(bg=error_back)
+            self.amount_error_label.config(text=error_feedback)
+
+        else:
+            # set starting balance to amount entered by user
+            self.starting_rounds.set(starting_rounds_set)
+            self.start_amount_entry.config(bg="#b7e4c7")
+
+    # to_game function
     def to_game(self):
-        # retrieve starting rounds
-        starting_rounds = 3
+        starting_rounds_set = self.starting_rounds.get()
 
-        Game(self, starting_rounds)
+        Game(self, starting_rounds_set)
 
         # hide start up window
-        self.start_frame.destroy()
+        root.withdraw()
 
 
 class Game:
-    def __init__(self, partner, starting_rounds):
+    def __init__(self, partner, starting_rounds_set):
 
 
         # initialise variables
         self.rounds = IntVar()
 
         # Set starting balance to amount entered by the user at the start if the game
-        self.rounds.set(starting_rounds)
+        self.rounds.set(starting_rounds_set)
 
         # List for holding stats
         self.round_stats_list = []
@@ -120,7 +196,7 @@ class Game:
                                    text=start_text, wrap=300, justify=LEFT)
         self.rounds_label.grid(row=0, column=0, pady=10)
 
-        self.rounds1_label = Label(self.rounds_frame, font="Arial 12 bold", fg="#80b918", text=rounds,
+        self.rounds1_label = Label(self.rounds_frame, font="Arial 12 bold", fg="#80b918", text=self.rounds,
                                    wrap=300, justify=LEFT)
         self.rounds_label.grid(row=0, column=1, pady=10)
 
@@ -166,11 +242,10 @@ class Game:
         self.capital_entry.delete(0, 'end')
         # change question label
         self.country_q_label.config(text="What is the capital of: ")
-        # round addition
-        self.rounds.set(1)
+        
 
         # for testing
-        print(rounds)
+        print(self.rounds)
 
         # Open csv file and get provided information
         with open("00_country_capital.csv") as f:
